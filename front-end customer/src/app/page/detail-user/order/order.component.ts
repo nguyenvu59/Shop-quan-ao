045ed25578575819516bcfc7ccdc0f402ef9c1f9
Quokka <76925113+nguyenvu59@users.noreply.github.com>
2024-05-02 16:30:10 +0700

    update detail

--- front-end customer/src/app/page/detail-user/order/order.component.ts
+++ front-end customer/src/app/page/detail-user/order/order.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { faEye, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { ToastrService } from 'ngx-toastr';
 import { listStatusOrder } from 'src/app/common/const';
 import { OrderService } from 'src/app/services/order.service';
 import { ProductService } from 'src/app/services/product.service';
 import { StorageService } from 'src/app/services/storage.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-order',
@@ -11,6 +13,10 @@ import { StorageService } from 'src/app/services/storage.service';
   styleUrls: ['./order.component.css'],
 })
 export class OrderComponent implements OnInit {
+  faEye = faEye;
+  faTimes = faTimes;
+  environment = environment;
+
 
   listOrder: any[] = [];
 
@@ -23,21 +29,36 @@ export class OrderComponent implements OnInit {
 
   ngOnInit() {
     this.getOrder();
-  }  
+  }
 
   getOrder() {
     this._orderService.orderController().search().subscribe(
       (res: any) => {
         this.listOrder = res.Data;
         this.listOrder.map((item: any) => {
-          item.status_name = listStatusOrder.find((obj:any)=>obj.id == item.status)?.name;
-          item.payment_method_name=item.payment_method == 'ThanhToanKhiNhanHang'?"Thanh toán khi nhận hàng":"Creat Card";
+          item.status_name = listStatusOrder.find((obj: any) => obj.id == item.status)?.name;
+          item.payment_method_name = item.payment_method == 'ThanhToanKhiNhanHang' ? "Thanh toán khi nhận hàng" : "Creat Card";
         })
       }
     ),
       (error: any) => {
         if (error?.Data) {
           this.toastr.error(error.error?.Data, "Thông báo");
+        }
+      }
+  }
+  showModal:boolean = false;
+  dataDetail:any[]=[];
+  itemOrder(item:any) {
+    this.showModal = !this.showModal;
+    this._orderService.orderController().getItem(item.id).subscribe(
+      (res: any) => {
+        this.dataDetail = res.Data.details;
+      }
+    ),
+      (error: any) => {
+        if (error?.Data) {
+          this.toastr.error(error?.Data, "Thông báo");
         }
       }
   }
